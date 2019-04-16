48f9bbcc8fe8e8b0daa7c419fe0b4e7124bcb105
tokinonagare <timemahou@gmail.com>
2019-04-16 10:54:27 +0800

    1904161054

--- js/default.thm_8fd7d3bf.js
+++ js/default.thm_8fd7d3bf.js
@@ -2026,7 +2026,7 @@ window.skins={};
 
 	function GameSkin() {
 		_super.call(this);
-		this.skinParts = ["icon","mainPotLabel","paymentMethodLabel","creatorLabel","houseNumberLabel","blindLabel","countdownLabel","roomInfoGroup","communityCardList","startGameButton","cardTypeLabel","potsList","startRoomButton","playGameButtonGroup","promptLabel","winningCardLabel","group","raiseSelector","messageAlarm","quickRaiser","reviewboardButton","menuButton","debugView","leaderboardButton","chatButton","microphoneButton","cardPattern"];
+		this.skinParts = ["icon","mainPotLabel","settlementMethodLabel","creatorLabel","houseNumberLabel","blindLabel","countdownLabel","roomInfoGroup","communityCardList","startGameButton","cardTypeLabel","potsList","startRoomButton","playGameButtonGroup","promptLabel","winningCardLabel","group","raiseSelector","messageAlarm","quickRaiser","reviewboardButton","menuButton","debugView","leaderboardButton","chatButton","microphoneButton","cardPattern"];
 		
 		this.height = 1334;
 		this.width = 750;
@@ -2087,12 +2087,12 @@ window.skins={};
 		t.touchThrough = true;
 		t.width = 400;
 		t.y = 820;
-		t.elementsContent = [this.paymentMethodLabel_i(),this.creatorLabel_i(),this.houseNumberLabel_i(),this.blindLabel_i(),this.countdownLabel_i()];
+		t.elementsContent = [this.settlementMethodLabel_i(),this.creatorLabel_i(),this.houseNumberLabel_i(),this.blindLabel_i(),this.countdownLabel_i()];
 		return t;
 	};
-	_proto.paymentMethodLabel_i = function () {
+	_proto.settlementMethodLabel_i = function () {
 		var t = new eui.Label();
-		this.paymentMethodLabel = t;
+		this.settlementMethodLabel = t;
 		t.height = 33;
 		t.horizontalCenter = 0;
 		t.scaleX = 1;
